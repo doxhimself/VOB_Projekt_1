ff66dc0f0bd827a6180948e0eb80cdef5a8440bc
Dominik Pohl <267584@vutbr.cz>
2026-03-16 13:56:21 +0100

    added .gitignore file and telemetry.csv

--- Project_1/obj/Debug/net10.0/Project_1.AssemblyInfo.cs
+++ Project_1/obj/Debug/net10.0/Project_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0a672887c5d3bc8a6214435e7391a54cd9bfb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
