280e229644cc32a5c9afdabdbba5293cb3847cd1
doxhimself <267584@vutbr.cz>
2026-03-18 17:53:39 +0100

    added the alarmclass

---
+++ Project_1/Alarm/AlarmClass.cs
@@ -0,0 +1,8 @@
+public class Alarm
+{
+    public DateTime Timestamp { get; set;}
+    public string MachineId { get; set;}
+    public string RuleName { get; set;}
+    public string Message { get; set;}
+
+}
