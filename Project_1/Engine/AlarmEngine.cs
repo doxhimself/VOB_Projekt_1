2de479381aa79ccad8e9becf03de73681755548b
doxhimself <267584@vutbr.cz>
2026-03-18 19:14:36 +0100

    nvm, now it should work

--- Project_1/Engine/AlarmEngine.cs
+++ Project_1/Engine/AlarmEngine.cs
@@ -3,7 +3,13 @@ public class AlarmEngine
     private readonly List<AlarmRule> _rules = new();
 
     public event EventHandler<Alarm>? AlarmTriggered;
-    
+
+    public void AddRule(AlarmRule rule)                                                                                
+  {                                                                                                                  
+      rule.AlarmTriggered += OnAlarmTriggered;                                                                       
+      _rules.Add(rule);                                                                                              
+  }                  
+
     public void Process(TelemetrySample sample)
     {
         foreach (var rule in _rules)
