63ff367560bfd6ee165aa5984484d226601c1eaf
doxhimself <267584@vutbr.cz>
2026-03-18 18:41:05 +0100

    added a RaiseAlarm, cause the subclasses werent able to invoke the event directly.

--- Project_1/Rules/AlarmRule.cs
+++ Project_1/Rules/AlarmRule.cs
@@ -1,5 +1,11 @@
 public abstract class AlarmRule
 {
-    public event EventHandler<Alarm> AlarmTriggered;
+    public event EventHandler<Alarm>? AlarmTriggered;
+    
     public abstract void Check(TelemetrySample sample);
+    protected void RaiseAlarm(Alarm alarm)
+    {
+        AlarmTriggered?.Invoke(this, alarm);
+    }
 }
+
