902586e0a8f1ece3a97f9d90f33db853d88a2f5a
doxhimself <267584@vutbr.cz>
2026-03-18 18:18:36 +0100

    get rid of duplicates

--- Project_1/Alarm/AlarmClass.cs
+++
@@ -1,8 +0,0 @@
-public class Alarm
-{
-    public DateTime Timestamp { get; set;}
-    public string MachineId { get; set;}
-    public string RuleName { get; set;}
-    public string Message { get; set;}
-
-}
