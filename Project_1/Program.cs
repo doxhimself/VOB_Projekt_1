d06f4efda753c73171f3bc570744565252a1d04c
doxhimself <267584@vutbr.cz>
2026-03-18 19:14:54 +0100

    as well as this

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+var engine = new AlarmEngine();
+engine.AddRule(new HighTemperatureRule(70.0));
+engine.AddRule(new HighVibrationRule(10.0));
+
+engine.AlarmTriggered += (sender, alarm) =>
+{
+    Console.WriteLine($"[{alarm.Timestamp}] ALARM on {alarm.MachineId} | {alarm.RuleName}: {alarm.Message}");
+};
+
+foreach (var sample in TelemetryReader.ReadTelemetrySamples("telemetry.csv"))
+{
+    engine.Process(sample);
+    Thread.Sleep(200);
+}
